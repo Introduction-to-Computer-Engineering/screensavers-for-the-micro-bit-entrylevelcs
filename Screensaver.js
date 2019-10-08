2518cd5bc0e5c83319cc407ef757fa2b7a8b5bd4
entrylevelcs <54680246+entrylevelcs@users.noreply.github.com>
2019-10-08 11:19:15 -0600

    Update Screensaver.js
    Added screensaver 2 and in order to change between screensavers you must now hold the A button until the screen clears then let go.

--- Screensaver.js
+++ Screensaver.js
@@ -3,14 +3,27 @@ let screenNumber = 0
 let x = 0
 let y = 0
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     isAsleep = true
-    screenNumber = 1//Math.randomRange(0, 1)
+    screenNumber = Math.randomRange(1, 2)
     //screensaver 1
     if (isAsleep && screenNumber == 1) {
         while (isAsleep && screenNumber == 1) {
             randomTog()
             if (input.buttonIsPressed(Button.A)) {
                 screenNumber = 0
+                isAsleep = false
+                basic.clearScreen()
+            }
+        }
+    }
+    //screensaver 2
+    if (isAsleep && screenNumber == 2) {
+        while (isAsleep && screenNumber == 2) {
+            rain()
+            if (input.buttonIsPressed(Button.A)) {
+                screenNumber = 0
+                isAsleep = false
                 basic.clearScreen()
             }
         }
@@ -24,8 +37,19 @@ input.onButtonPressed(Button.B, function () {
 })
 
 function randomTog() {
+    led.toggle(Math.randomRange(0, 4), Math.randomRange(0, 4))
+    basic.pause(30)
+}
+function rain() {
+    basic.clearScreen()
     x = Math.randomRange(0, 4)
-    y = Math.randomRange(0, 4)
-    led.toggle(x, y)
-    basic.pause(50)
+    led.toggle(x, 0)
+    basic.pause(100)
+    led.toggle(x, 0)
+    led.toggle(x, 2)
+    basic.pause(100)
+    led.toggle(x, 2)
+    led.toggle(x, 4)
+    basic.pause(100)
+    led.toggle(x, 4)
 }
